d160b15f913dbdee7f73f67969fd6298f52a3888
ismail5g <ismail27.dec@gmail.com>
2020-07-14 22:20:41 +0600

    Added Attendees

--- client-app/src/Features/Activities/Dashboard/ActivityListItem.tsx
+++ client-app/src/Features/Activities/Dashboard/ActivityListItem.tsx
@@ -3,6 +3,7 @@ import { Item, Button, Segment, Icon} from 'semantic-ui-react'
 import { Link } from 'react-router-dom';
 import { IActivity } from '../../../App/Models/Activity';
 import {format} from 'date-fns';
+import ActivityListItemAttendees from './ActivityListItemAttendees';
 
 const ActivityListItem: React.FC<{activity: IActivity}> = ({activity}) => {
 
@@ -26,7 +27,9 @@ const ActivityListItem: React.FC<{activity: IActivity}> = ({activity}) => {
                     <Icon name='clock' /> {format(activity.date, 'h:mm a')}
                     <Icon name='marker' /> {activity.venue}, {activity.city}
                 </Segment>
-                <Segment secondary>Attendees will go here</Segment>
+                <Segment secondary>
+                    <ActivityListItemAttendees attendees = {activity.Attendees} />
+                </Segment>
                 <Segment clearing>
                     <span>{activity.description}</span>
                     <Button
---
+++ client-app/src/Features/Activities/Dashboard/ActivityListItemAttendees.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { IAttendee } from "../../../App/Models/Activity";
+import { List, Popup, Image } from "semantic-ui-react";
+
+interface IProps {
+  attendees: IAttendee[];
+}
+
+const ActivityListItemAttendees: React.FC<IProps> = ({ attendees }) => {
+  console.log(attendees);
+  return (
+    <List horizontal>
+      {attendees &&
+        attendees.map((attendee) => (
+          <List.Item key={attendee.username}>
+            <Popup
+              header={attendee.displayName}
+              trigger={
+                <Image
+                  size="mini"
+                  circular
+                  src={attendee.image || `/items/user.png`}
+                />
+              }
+            />
+          </List.Item>
+        ))}
+    </List>
+  );
+};
+
+export default ActivityListItemAttendees;
