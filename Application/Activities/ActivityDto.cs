04c1bb0988b1993f14bf621039db3c1fbfc6fb5a
ismail5g <ismail27.dec@gmail.com>
2020-08-01 21:31:28 +0600

    Added Comment Entity and CommentDto

--- Application/Activities/ActivityDto.cs
+++ Application/Activities/ActivityDto.cs
@@ -1,5 +1,7 @@
 using System;
+using System.Collections;
 using System.Collections.Generic;
+using Application.Comments;
 using Newtonsoft.Json;
 
 namespace Application.Activities
@@ -16,5 +18,6 @@ namespace Application.Activities
         public string Venue { get; set; }
         [JsonProperty("Attendees")]
         public ICollection<AttendeeDto> UserActivities { get; set; }
+        public ICollection<CommentDto> Comments { get; set; }
     }
 }
