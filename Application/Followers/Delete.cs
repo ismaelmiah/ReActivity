7160861ed1e029e157c5fc398e1c5eba3700c520
ismail5g <ismail27.dec@gmail.com>
2020-08-04 11:29:03 +0600

    Follow & Unfollow Done

---
+++ Application/Followers/Delete.cs
@@ -0,0 +1,54 @@
+using System.Net;
+using System.Threading;
+using System.Threading.Tasks;
+using Application.Errors;
+using Application.Interfaces;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+
+namespace Application.Followers
+{
+    public class Delete
+    {
+        public class Command : IRequest
+        {
+            public string Username { get; set; }
+        }
+
+        public class Handler : IRequestHandler<Command>
+        {
+            private readonly DataContext _context;
+            private readonly IUserAccessor _userAccessor;
+            public Handler(DataContext context, IUserAccessor userAccessor)
+            {
+                _userAccessor = userAccessor;
+                _context = context;
+            }
+
+            public async Task<Unit> Handle(Command request, CancellationToken cancellationToken)
+            {
+                var observer = await _context.Users.SingleOrDefaultAsync(x => x.UserName == _userAccessor.GetCurrentUsername());
+
+                var target = await _context.Users.SingleOrDefaultAsync(x => x.UserName == request.Username);
+
+                if(target == null) throw new RestException(HttpStatusCode.NotFound, new {User = "Not Found"});
+
+                var followings = await _context.Followings.SingleOrDefaultAsync(x => x.ObserverId == observer.Id && x.TargetId == target.Id);
+
+                if(followings == null) throw new RestException(HttpStatusCode.BadRequest, new {User = "You are not following this user"});
+
+                if(followings != null)
+                {
+                    _context.Followings.Remove(followings);
+                }
+
+                var success = await _context.SaveChangesAsync() > 0;
+
+                if(success) return Unit.Value;
+
+                throw new System.Exception("Problem Saving Changes");
+            }
+        }
+    }
+}
