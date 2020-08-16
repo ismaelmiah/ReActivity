81e75390014bf71b7594c8650ad8c4bf682577fe
ismail5g <ismail27.dec@gmail.com>
2020-08-16 21:46:11 +0600

    set Environment Variable

--- client-app/src/App/Api/Agent.ts
+++ client-app/src/App/Api/Agent.ts
@@ -5,7 +5,7 @@ import { toast } from "react-toastify";
 import { IUser, IUserFormValue } from "../Models/User";
 import { IProfile, IPhoto } from "../Models/Profile";
 
-axios.defaults.baseURL = "http://localhost:5000";
+axios.defaults.baseURL = process.env.REACT_APP_API_URL;
 
 axios.interceptors.request.use(
   (config) => {
@@ -47,18 +47,16 @@ axios.interceptors.response.use(undefined, (error) => {
 
 const responseBody = (response: AxiosResponse) => response.data;
 
-const sleep = (ms: number) => (response: AxiosResponse) =>
-  new Promise<AxiosResponse>((resolve) =>
-    setTimeout(() => resolve(response), ms)
-  );
+// const sleep = (ms: number) => (response: AxiosResponse) =>
+//   new Promise<AxiosResponse>((resolve) =>
+//     setTimeout(() => resolve(response), ms)
+//   );
 
 const requests = {
-  get: (url: string) => axios.get(url).then(sleep(10)).then(responseBody),
-  post: (url: string, body: {}) =>
-    axios.post(url, body).then(sleep(1000)).then(responseBody),
-  put: (url: string, body: {}) =>
-    axios.put(url, body).then(sleep(1000)).then(responseBody),
-  del: (url: string) => axios.delete(url).then(sleep(1000)).then(responseBody),
+  get: (url: string) => axios.get(url).then(responseBody),
+  post: (url: string, body: {}) => axios.post(url, body).then(responseBody),
+  put: (url: string, body: {}) => axios.put(url, body).then(responseBody),
+  del: (url: string) => axios.delete(url).then(responseBody),
   postform: (url: string, file: Blob) => {
     let formData = new FormData();
     formData.append("File", file);
@@ -74,7 +72,6 @@ const Activities = {
   list: (params: URLSearchParams): Promise<IActivitesEnvelope> =>
     axios
       .get("/activities", { params: params })
-      .then(sleep(1000))
       .then(responseBody),
   details: (id: string) => requests.get(`/activities/${id}`),
   create: (activity: IActivity) => requests.post("/activities", activity),
